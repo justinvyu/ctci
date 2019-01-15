23ee834e19d9af7fb3895577dda2a08b4ff85e73
justinvyu <justin.v.yu@gmail.com>
2019-01-14 23:56:24 -0800

    Add ch04 exercises

--- exercises/chapter04.py
+++ exercises/chapter04.py
@@ -5,6 +5,8 @@ import sys
 sys.path.insert(0, "../data-structures/")
 from graph import Graph
 from queue import Queue
+from tree import BinaryTree
+from linkedlist import Node
 
 # 4.1 Route Between Nodes
 
@@ -47,5 +49,43 @@ def minimal_tree(lst):
     """
     Returns a binary search tree with minimal height containing the numbers
     passed in as a sorted array of unique integers.
+    >>> lst = [1, 2, 3, 4, 5, 6, 7]
+    >>> print(minimal_tree(lst))
+    4
+      2
+        1
+        3
+      6
+        5
+        7
     """
+    if len(lst) == 1:
+        return BinaryTree(lst[0])
+    elif not lst:
+        return BinaryTree.empty
+    else:
+        mid = len(lst) // 2
+        left = minimal_tree(lst[:mid])
+        right = minimal_tree(lst[mid+1:])
+        return BinaryTree(lst[mid], left, right)
 
+# 4.3 List of Depths
+
+def list_of_depths(tree):
+    """
+    Returns a list of D linked lists with the elements of each level in the
+    binary tree, where D is the total number of levels/the depth.
+    >>> list_of_depths(minimal_tree(list(range(1, 8))))
+    [Node(4), Node(6, Node(2)), Node(7, Node(5, Node(3, Node(1))))]
+    """
+    depths = {}
+    def add_node_at_depth(t, depth=0):
+        if t is BinaryTree.empty:
+            return
+        if depth not in depths:
+            depths[depth] = Node.empty
+        depths[depth] = Node(t.label, depths[depth])
+        add_node_at_depth(t.left, depth + 1)
+        add_node_at_depth(t.right, depth + 1)
+    add_node_at_depth(tree)
+    return list(depths.values())
